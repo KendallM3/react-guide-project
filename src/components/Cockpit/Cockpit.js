e76c69dc07ef8afcea1703b07bc161bbb9554696
Kendall MacMillan <kendall.macmillan@newtoneurope.com>
2020-08-30 00:01:52 +0100

    section 7: 111

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
 
+    const toggleBtnRef = useRef(null);
+
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // http request can go here
-        setTimeout(() => {
-            alert('Saved data to cloud');
-        },1000);
+        // setTimeout(() => {
+        //     alert('Saved data to cloud');
+        // },1000);
+        toggleBtnRef.current.click();
         return () => {
             console.log('[Cockpit.js] cleanup work in useEffect');
         };
@@ -41,8 +45,11 @@ const cockpit = (props) => {
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass}
-            onClick={props.clicked}>
+            <button 
+                ref={toggleBtnRef}
+                className={btnClass}
+                onClick={props.clicked}
+            >
                 Toggle People
             </button>
         </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -24,7 +24,8 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   }
 
   static getDerivedStateFromProps(props, state) {
@@ -66,8 +67,14 @@ class App extends Component {
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
-    this.setState({persons: persons});    
-  };
+    this.setState((prevState, props) => {
+      return {
+        persons: persons, 
+        changeCounter: prevState.changeCounter + 1
+      };
+    });
+  
+  }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
