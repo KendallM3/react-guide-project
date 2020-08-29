d412f2f070d6bfa517ce6b18a0bdf3387e34c0bf
Kendall MacMillan <kendall.macmillan@newtoneurope.com>
2020-08-29 19:44:33 +0100

    section 7: 106

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
-import WithClass from '../hoc/WithClass';
+import withClass from '../hoc/withClass';
+import Aux from '../hoc/Auxiliary';
 // import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 
@@ -91,7 +92,7 @@ class App extends Component {
 
 
     return (
-      <WithClass classes={classes.App}>
+      <Aux>
         <button onClick={() => {
           this.setState({showCockpit: false});
           }}
@@ -105,10 +106,10 @@ class App extends Component {
         clicked={this.togglePersonsHandler}
         /> : null }
         {persons}
-      </WithClass>
+        </Aux>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
